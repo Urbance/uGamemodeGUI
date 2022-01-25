cb4829e6933f990b62d869a862b571752126e3ba
Martin <schmidtmartin697@gmail.com>
2022-01-25 20:46:41 +0100

    Replaced Bukkit.getConsoleSender().sendMessage() to getLogger().info

--- src/main/java/de/urbance/main/Main.java
+++ src/main/java/de/urbance/main/Main.java
@@ -17,12 +17,11 @@ public final class Main extends JavaPlugin {
     public static File gui = new File("plugins//uGamemodeGUI//gui.yml");
     public static File config = new File("plugins//uGamemodeGUI//config.yml");
     public static YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(gui);
-    private static String cmdprefix = "§7[§cuGamemodeGUI§7] ";
     public static String prefix = "&7[&cuGamemodeGUI&7] ";
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage(cmdprefix + "Starting up..");
+        getLogger().info("Starting up..");
         getServer().getPluginManager().registerEvents(new Listeners(), this);
 
         getCommand("gmgui").setExecutor(new GmGUI());
@@ -43,7 +42,7 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage(cmdprefix + "Shutdown..");
+        getLogger().info("Shutdown..");
     }
 
     public void loadGuiConfiguration() {
